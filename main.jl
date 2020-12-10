9988be6e96fafaca8847ccaafc301015bee69171
LeonardoBeraldi <74253812+LeonardoBeraldi@users.noreply.github.com>
2020-12-10 14:32:47 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -64,7 +64,7 @@ y_pred = modelo.(x1)
 y_med = mean(z)
 R2 = 1 - norm(y_pred - z)^2 / norm(y_med .- z)^2
 
-scatter(x1,z, c=:blue, ms=3, leg=false) #O intervalo de x está como se os meses fossem 10 meses.
+scatter(x1,z, c=:blue, ms=3, leg=false) 
 plot!(modelo, c=:red, lw=2, xlabel="Anos", ylabel="Qtd. de Água Reservatórios (%)", xlim = (2008.9,2020.5))
 title!("R2 = $R2")
 
@@ -75,10 +75,19 @@ y_med = mean(z2)
 R2 = 1 - norm(y_pred - z2)^2 / norm(y_med .- z2)^2
 
 scatter(x2,z2, c=:blue, ms=3, leg=false) 
-plot!(modelo, c=:red, lw=2, xlabel="Ano", ylabel="Qtd. de Água Reservatórios (%)", xlim = (2009,2020.5))   #O intervalo de x está como se os meses fossem 10 meses.
+plot!(modelo, c=:red, lw=2, xlabel="Ano", ylabel="Qtd. de Água Reservatórios (%)", xlim = (2009,2020.5))  
 title!("R2 = $R2")
 
-F = [x->1, x->x, x-> sqrt(x), x->sin(2π/100000 * x), x->cos(2π/100000 * x)]  # Modelo para  População X Consumo (Reta)
+
+F = [x->1, x->x]  # Modelo para  População X Consumo (Reta)
+
+β, modelo = regressao(w, y, F)
+y_pred = modelo.(w)
+y_med = mean(y)
+R2 = 1 - norm(y_pred - y)^2 / norm(y_med .- y)^2
+
+
+F = [x->1, x->x, x-> sqrt(x), x->sin(2π/100000 * x), x->cos(2π/100000 * x)]  # Modelo para  População X Consumo que melhor se adapta
 
 β, modelo = regressao(w, y, F)
 y_pred = modelo.(w)
@@ -89,11 +98,11 @@ scatter(w, y, c=:blue, ms=3, leg=false)
 plot!(modelo, c=:red, lw=2, xlabel="Qtd. da População", ylabel="Consumo de Água (litros)", xlim = (1502000,1907000))
 title!("R2 = $R2")
 
-a = 1:16                                                            # Modelo Meses (Julho de 2019 a Outubro de 2020)  X Reservatório
+a = 1:16                                                            # Modelo Meses (Julho de 2019 a Outubro de 2020)  X %Reservatório
 b = [99.25, 95.5, 95.5, 93.75, 95.5, 89, 83.75, 80.5, 81.75, 61.75, 50.75, 
      45.75, 40, 40, 34, 31]
 
-F = [x->1, x->x, x->x^2, x->cos(2π/11*x), x->sin(2π/11*x)]      #Acrescentado um sin e cos.
+F = [x->1, x->x, x->x^2, x->cos(2π/11*x), x->sin(2π/11*x)]      
 
 β, modelo = regressao(a, b, F)
 y_pred = modelo.(a)
