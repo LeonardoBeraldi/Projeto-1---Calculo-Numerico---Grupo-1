136b5cc7bce32a20c697fa2ad8ab0ea0293af0eb
HiroshiTA <74251623+HiroshiTA@users.noreply.github.com>
2020-12-10 00:30:26 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -41,7 +41,7 @@ y_med = mean(y)
 R2 = 1 - norm(y_pred - y)^2 / norm(y_med .- y)^2
 
 scatter(x, y, c=:blue, ms=3, leg=false)
-plot!(modelo, c=:red, lw=2, xlim = (1996,2020))
+plot!(modelo, c=:red, lw=2, xlabel="Anos", ylabel="Qtd. Consumida", xlim = (1996,2020))
 title!("R2 = $R2")
 
 F = [x->1, x -> x, x->-x^2, x->x^3, x-> sin(2π/12 *x), x->cos(2π/12*x)]   #Modelo que melhor descreve Anos X Consumo que encontramos
@@ -52,7 +52,7 @@ y_med = mean(y)
 R2 = 1 - norm(y_pred - y)^2 / norm(y_med .- y)^2
 
 scatter(x, y, c=:blue, ms=3, leg=false)
-plot!(modelo, c=:red, lw=2, xlim = (1996,2020))
+plot!(modelo, c=:red, lw=2, xlabel="Anos", ylabel="Qtd. Consumida", xlim = (1996,2020))
 title!("R2 = $R2")
 
 F = [x -> 1, x-> x^2, x-> x^3, x-> sin(2π/4*x), x->cos(2π/4*x), x-> sin(2π/5*x), x->cos(2π/5*x), 
@@ -64,8 +64,8 @@ y_pred = modelo.(x1)
 y_med = mean(z)
 R2 = 1 - norm(y_pred - z)^2 / norm(y_med .- z)^2
 
-scatter(x1,z, c=:blue, ms=3, leg=false) 
-plot!(modelo, c=:red, lw=2, xlim = (2009,2020))
+scatter(x1,z, c=:blue, ms=3, leg=false) #O intervalo de x está como se os meses fossem 10 meses.
+plot!(modelo, c=:red, lw=2, xlabel="Anos", ylabel="Qtd. de Água Reservatórios (%)", xlim = (2008.9,2020.5))
 title!("R2 = $R2")
 
 x2, z2 = x1[1:11], z[1:11]
@@ -75,10 +75,10 @@ y_med = mean(z2)
 R2 = 1 - norm(y_pred - z2)^2 / norm(y_med .- z2)^2
 
 scatter(x2,z2, c=:blue, ms=3, leg=false) 
-plot!(modelo, c=:red, lw=2, xlim = (2009,2050))
+plot!(modelo, c=:red, lw=2, xlabel="Ano", ylabel="Qtd. de Água Reservatórios (%)", xlim = (2009,2020.5))   #O intervalo de x está como se os meses fossem 10 meses.
 title!("R2 = $R2")
 
-F = [x->1, x->x]            # Modelo para  População X Consumo (Reta)
+F = [x->1, x->x, x-> sqrt(x), x->sin(2π/100000 * x), x->cos(2π/100000 * x)]  # Modelo para  População X Consumo (Reta)
 
 β, modelo = regressao(w, y, F)
 y_pred = modelo.(w)
@@ -86,14 +86,14 @@ y_med = mean(y)
 R2 = 1 - norm(y_pred - y)^2 / norm(y_med .- y)^2
 
 scatter(w, y, c=:blue, ms=3, leg=false) 
-plot!(modelo, c=:red, lw=2, xlim = (1600000,1900000))
+plot!(modelo, c=:red, lw=2, xlabel="Qtd. da População", ylabel="Consumo de Água (litros)", xlim = (1502000,1907000))
 title!("R2 = $R2")
-   
+
 a = 1:16                                                            # Modelo Meses (Julho de 2019 a Outubro de 2020)  X Reservatório
 b = [99.25, 95.5, 95.5, 93.75, 95.5, 89, 83.75, 80.5, 81.75, 61.75, 50.75, 
      45.75, 40, 40, 34, 31]
 
-F = [x->1, x->x, x->x^2]      
+F = [x->1, x->x, x->x^2, x->cos(2π/11*x), x->sin(2π/11*x)]      #Acrescentado um sin e cos.
 
 β, modelo = regressao(a, b, F)
 y_pred = modelo.(a)
@@ -101,6 +101,6 @@ y_med = mean(b)
 R2 = 1 - norm(y_pred - b)^2 / norm(y_med .- b)^2
 
 scatter(a, b, c=:blue, ms=3, leg=false) 
-plot!(modelo, c=:red, xlabel="Qtd. de Chuva mês a mês (07/2019 - 10/2020)",
-    ylabel = "Qtd. de Água Reservatórios ()", lw=2, xlim = (0,18))
+plot!(modelo, c=:red, xlabel="Qtd. de Chuva (07/2019 - 10/2020)",
+    ylabel = "Qtd. de Água nos Reservatórios (%)", lw=2, xlim = (0,17))
 title!("R2 = $R2")
